b6e681680822e1ef983f571fe9a9cc347a6126d0
IgnOry <iory@ucm.es>
2021-01-22 15:05:24 +0100

    SaveGame?

--- Practica 3/Assets/Scripts/GameManager.cs
+++ Practica 3/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -17,6 +18,8 @@ public class GameManager : MonoBehaviour
     bool _isPaused = false;
     
     private static GameManager _instance;
+
+    private gameSaveData gameSaveData_;
 
     public static GameManager Instance()
     {
@@ -57,11 +60,14 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject); //Persistente entre escenas
 
             _completedLevel = new int[_levelPacks.Length];
-
+            /*
             for (int i = 0; i < _levelPacks.Length; i++)
             {
                 _completedLevel[i] = 0;
             }
+            */
+
+            loadData();
         }
     }
 
@@ -182,6 +188,8 @@ public class GameManager : MonoBehaviour
         {
             Pause();
 
+            saveData(); //Tampoco se si va aqui
+
             _levelManager.LevelFinished();
         }
     }
@@ -199,6 +207,7 @@ public class GameManager : MonoBehaviour
     public void AddHintAd(UnityEngine.Advertisements.ShowResult show)
     {
         AddHint();
+        saveData();
     }
 
     public void FreeHint(UnityEngine.Advertisements.ShowResult show)
@@ -211,5 +220,58 @@ public class GameManager : MonoBehaviour
     public void AddHint()
     {
         _hints++;
+    }
+
+    //SAVE DATA STUFF
+    [Serializable]
+    protected class gameSaveData
+    {
+        public int hints;
+        public int[] packs = new int[50]; //Es un valor arbitrario, pero siendo packs, no se qué valor poner para no quedarnos cortos
+    }
+
+    protected class gameSaveDataHASH
+    {
+        public int hashCode;
+        public string json;
+    }
+
+    public void saveData()
+    {
+        gameSaveData_.hints = GameManager._instance._hints;
+        gameSaveData_.packs = GameManager._instance._completedLevel;
+
+        string stringSaveData = JsonUtility.ToJson(gameSaveData_);
+        gameSaveDataHASH hashData = new gameSaveDataHASH();
+        hashData.json = stringSaveData;
+        hashData.hashCode = stringSaveData.GetHashCode();
+
+        string aux = JsonUtility.ToJson(hashData);
+        PlayerPrefs.SetString("SaveData", aux);
+    }
+
+    public void loadData()
+    {
+        string codeToVerify = PlayerPrefs.GetString("SaveData");
+        gameSaveDataHASH hashData = JsonUtility.FromJson<gameSaveDataHASH>(codeToVerify);
+
+        if (hashData != null)
+        {
+            if (hashData.hashCode == hashData.json.GetHashCode())
+            {
+                gameSaveData gameSaveData = JsonUtility.FromJson<gameSaveData>(hashData.json);
+                GameManager._instance._hints = gameSaveData.hints;
+                GameManager._instance._completedLevel = gameSaveData.packs;
+            }
+        }
+        else //Si haces trampas o no hay nada
+        {
+            GameManager._instance._hints = 0;
+
+            for (int i = 0; i < GameManager._instance._completedLevel.Length; i++)
+            {
+                GameManager._instance._completedLevel[i] = 1; //No se que valor es por defecto, si 1 o 0
+            }
+        }
     }
 }
